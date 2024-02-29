e4f8926db9223ad3c05aeb508031ce7f17a8a26e
Lucas Ramadan <lucasramadan@gmail.com>
2024-02-28 17:27:14 -0800

    create barebones main section

---
+++ src/components/CoreAssembly.tsx
@@ -0,0 +1,12 @@
+import CoreTile from './CoreTile'
+
+const CoreAssembly = () => {
+  return (
+    <main>
+      <CoreTile />
+    </main>
+
+  )
+}
+
+export default CoreAssembly
---
+++ src/components/CoreTile.tsx
@@ -0,0 +1,11 @@
+
+const CoreTile = (props) => {
+  return (
+    <div className="core-pannel">
+      <img src="/assets/core_logo.png" alt="grow-core" width="90px" />
+      <strong className="healthy-core">72°F</strong>
+    </div>
+  )
+}
+
+export default CoreTile
