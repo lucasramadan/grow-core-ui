9cb351281c9f6b819f97f8f94e6ad9e25f2ee4f8
Lucas Ramadan <lucasramadan@gmail.com>
2024-02-28 16:58:14 -0800

    create Metric aside scaffolding

---
+++ src/components/Metric.tsx
@@ -0,0 +1,13 @@
+const Metric = () => {
+  return (
+    <div className="metric">
+      <img 
+        src='/metrics/lightbulb.png'
+        alt='Lightbulb'
+      />
+      <h6>Light</h6>
+    </div>
+  )
+}
+
+export default Metric
---
+++ src/components/MetricSelector.tsx
@@ -0,0 +1,13 @@
+import Metric from "./Metric"
+
+const MetricSelector = () => {
+  return (
+    <aside>
+      <h5 id="metric-head">Metrics</h5>
+      <Metric />
+    </aside>
+
+  )
+}
+
+export default MetricSelector
