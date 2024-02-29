776507d71e2d64fed95fa71420f0f3be6bfbc2e8
Lucas Ramadan <lucasramadan@gmail.com>
2024-02-29 15:20:05 -0800

    add multiple additional metrics

--- src/metricList.js
+++ src/metricList.js
@@ -1,9 +1,51 @@
 const metricList = [
   {
     id: 1,
+    name: 'Temperature',
+    alt: 'Thermometer',
+    logoPath: '/metrics/temperature.png'
+  },
+  {
+    id: 2,
+    name: 'Humidity',
+    alt: 'Waterdroplet Percentage',
+    logoPath: '/metrics/humidity.png'
+  },
+  {
+    id: 3,
     name: 'Light',
     alt: 'Lightbulb',
     logoPath: '/metrics/lightbulb.png'
+  },
+  {
+    id: 4,
+    name: 'CO2',
+    alt: 'CO2 Cloud',
+    logoPath: '/metrics/co2.png'
+  },
+  {
+    id: 5,
+    name: 'Vapor',
+    alt: 'Steam',
+    logoPath: '/metrics/water-vapor.png'
+  },
+  {
+    id: 6,
+    name: 'Airflow',
+    alt: 'Airflow',
+    logoPath: '/metrics/air-flow.png'
+  },
+  {
+    id: 7,
+    name: 'EC',
+    alt: 'Waterdroplet Electricity',
+    logoPath: '/metrics/ec_icon.png'
+  },
+  {
+    id: 8,
+    name: 'pH',
+    alt: 'pH Meter',
+    logoPath: '/metrics/ph.png'
   }
 ]
 
