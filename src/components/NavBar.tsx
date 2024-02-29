fb7604eeb7ab2290d321c03fba70755f7c815ba7
Lucas Ramadan <lucasramadan@gmail.com>
2024-02-28 16:50:55 -0800

    correct path issue with NavBar images

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import NavBarItem from './NavBarItem'
 const NavBar = () => {
   return (
     <nav>
-      <img id="logo" src="../assets/almanac.svg" alt="" width="150px" />
+      <img id="logo" src="/assets/almanac.svg" alt="" width="150px" />
       <NavBarItem itemName='Greenhouses'/>
       <NavBarItem itemName='Metrics'/>
       <NavBarItem itemName='Configurations'/>
       <NavBarItem itemName='Users'/>
-      <img id="user" src="../assets/user.png" alt="" width="30px" />
+      <img id="user" src="/assets/user.png" alt="" width="30px" />
     </nav>
 
   )
